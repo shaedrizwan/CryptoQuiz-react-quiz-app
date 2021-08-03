403da6ffdc0f340e590d3ee60646458318e8b3a7
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-03 22:34:19 +0530

    Feat: Components index added

--- cryptoquiz/src/Components/index.ts
+++ cryptoquiz/src/Components/index.ts
@@ -1,5 +1,9 @@
 import Header from "./Header/header";
 import { Footer } from "./Footer/footer";
+import { QuizComponent } from "./Quiz/quiz";
+import { HeroComponent } from "./Hero/HeroComponent";
+import { QuizListComponent } from "./QuizList/QuizListComponent";
+import { ResultComponent } from "./Result/ResultComponent";
 
 export
- {Header, Footer}
+ {Header, Footer, QuizComponent, HeroComponent, QuizListComponent, ResultComponent}
