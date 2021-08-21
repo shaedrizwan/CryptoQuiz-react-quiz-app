c143e292398f51ea93914f1baadcf3b5201a229b
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-21 22:49:49 +0530

    Upate: Component index

--- cryptoquiz/src/Components/index.ts
+++ cryptoquiz/src/Components/index.ts
@@ -4,6 +4,9 @@ import { QuizComponent } from "./Quiz/quiz";
 import { HeroComponent } from "./Hero/HeroComponent";
 import { QuizListComponent } from "./QuizList/QuizListComponent";
 import { ResultComponent } from "./Result/ResultComponent";
+import { RulesComponent } from "./Rules/RulesComponent";
+import { Leaderboard } from "./Leaderboard/Leaderboard";
+import { LoginComponent } from "./Login/LoginComponent";
 
 export
- {Header, Footer, QuizComponent, HeroComponent, QuizListComponent, ResultComponent}
+ {Header, Footer, QuizComponent, HeroComponent, QuizListComponent, ResultComponent, RulesComponent, Leaderboard, LoginComponent}
