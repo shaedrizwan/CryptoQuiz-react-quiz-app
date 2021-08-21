d7982a803cdf035b35de822463ed97b00a0499c5
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-21 22:47:19 +0530

    Feat: Added confetti for scores above 30

--- cryptoquiz/src/Components/Result/ResultComponent.tsx
+++ cryptoquiz/src/Components/Result/ResultComponent.tsx
@@ -1,12 +1,22 @@
 
 import { useScore } from "../../Contexts/scoreContext"
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 import "./ResultComponent.css"
 
 export function ResultComponent(){
-    const {score} = useScore()
+    const {score}:{score:number} = useScore()
+    const { width, height } = useWindowSize()
     return(
         <div className="result-container">
-            {score > 30 && <h2>Congratulations!🎉 Your score is: <span className="score">{score}</span></h2>}
+            {score > 30 && 
+                <div>
+                    <h2>Congratulations!🎉 Your score is: <span className="score">{score}</span></h2>
+                    <Confetti
+                    width={width}
+                    height={height}
+                    />
+                </div>}
             {score <= 30 && <h2> Your score is <span className="score">{score}</span>.Play again to improve your score</h2>}
         </div>
     )
